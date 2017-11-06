4025b2833d70c1f6a4bb2c52f7272e9bc2244359
fuentesj <jonathan.wilfredo.fuentes@gmail.com>
2017-11-05 20:38:02 -0800

    1. Fix GameGridTest.java

--- src/test/java/GameGridTest.java
+++ src/test/java/GameGridTest.java
@@ -1,63 +1,67 @@
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static junit.framework.TestCase.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-///**
-// * Created by Jonathan on 10/24/17.
-// */
-//public class GameGridTest {
-//
-//    private GameGrid gameGrid;
-//
-//    @Before
-//    public void setUp() {
-//        gameGrid = new GameGrid();
-//    }
-//
-//    @Test
-//    public void isGameNotOverWhenThereAreNoMarksOnGameGrid() {
-//        assertFalse(gameGrid.isGameOver());
-//    }
-//
-//    @Test
-//    public void isGameOverWhenThereAreThreeMarksInOneRow() {
-//        gameGrid.markGameCell(0,0, "X");
-//        gameGrid.markGameCell(0, 1, "X");
-//        gameGrid.markGameCell(0, 2, "X");
-//        assertTrue(gameGrid.isGameOver());
-//    }
-//
-//    @Test
-//    public void isGameOverWhenThereAreThreeMarksInOneColumn() {
-//        gameGrid.markGameCell(0,0, "X");
-//        gameGrid.markGameCell(1, 0, "X");
-//        gameGrid.markGameCell(2, 0, "X");
-//        assertTrue(gameGrid.isGameOver());
-//    }
-//
-//    @Test
-//    public void isGameOverWhenThereAreThreeMarksInOneDiagonal() {
-//        gameGrid.markGameCell(0,0, "X");
-//        gameGrid.markGameCell(1, 1, "X");
-//        gameGrid.markGameCell(2, 2, "X");
-//        assertTrue(gameGrid.isGameOver());
-//    }
-//
-//    @Test
-//    public void isGameOverWhenThereIsDraw() {
-//        gameGrid.markGameCell(0,0, "X");
-//        gameGrid.markGameCell(0, 1, "O");
-//        gameGrid.markGameCell(0, 2, "O");
-//
-//        gameGrid.markGameCell(1,0, "O");
-//        gameGrid.markGameCell(1, 1, "X");
-//        gameGrid.markGameCell(1, 2, "X");
-//
-//        gameGrid.markGameCell(2,0, "O");
-//        gameGrid.markGameCell(2, 1, "X");
-//        gameGrid.markGameCell(2, 2, "O");
-//        assertTrue(gameGrid.isDraw());
-//    }
-//}
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Jonathan on 10/24/17.
+ */
+public class GameGridTest {
+
+    private GameEngine gameEngine;
+    private GameStrategy strategy;
+    private GameGrid gameGrid;
+
+    @Before
+    public void setUp() {
+        gameEngine = new GameEngine();
+        strategy = new EasyGameStrategy(gameEngine);
+        gameGrid = new GameGrid(strategy, gameEngine);
+    }
+
+    @Test
+    public void isGameNotOverWhenThereAreNoMarksOnGameGrid() {
+        assertFalse(gameGrid.isGameOver());
+    }
+
+    @Test
+    public void isGameOverWhenThereAreThreeMarksInOneRow() {
+        gameGrid.markGameCell(0, 0, "X");
+        gameGrid.markGameCell(0, 1, "X");
+        gameGrid.markGameCell(0, 2, "X");
+        assertTrue(gameGrid.isGameOver());
+    }
+
+    @Test
+    public void isGameOverWhenThereAreThreeMarksInOneColumn() {
+        gameGrid.markGameCell(0, 0, "X");
+        gameGrid.markGameCell(1, 0, "X");
+        gameGrid.markGameCell(2, 0, "X");
+        assertTrue(gameGrid.isGameOver());
+    }
+
+    @Test
+    public void isGameOverWhenThereAreThreeMarksInOneDiagonal() {
+        gameGrid.markGameCell(0, 0, "X");
+        gameGrid.markGameCell(1, 1, "X");
+        gameGrid.markGameCell(2, 2, "X");
+        assertTrue(gameGrid.isGameOver());
+    }
+
+    @Test
+    public void isGameOverWhenThereIsDraw() {
+        gameGrid.markGameCell(0, 0, "X");
+        gameGrid.markGameCell(0, 1, "O");
+        gameGrid.markGameCell(0, 2, "O");
+
+        gameGrid.markGameCell(1, 0, "O");
+        gameGrid.markGameCell(1, 1, "X");
+        gameGrid.markGameCell(1, 2, "X");
+
+        gameGrid.markGameCell(2, 0, "O");
+        gameGrid.markGameCell(2, 1, "X");
+        gameGrid.markGameCell(2, 2, "O");
+        assertTrue(gameGrid.isDraw());
+    }
+}
